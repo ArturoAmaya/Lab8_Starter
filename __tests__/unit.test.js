28708ef01d16c83df835921dcea5bbd48f4758d6
sschaeff2023 <sschaeff2023@users.noreply.github.com>
2021-11-11 20:29:06 -0800

    strings

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,19 +3,19 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 test('checks first valid phone number', () => {
-    expect(functions.isPhoneNumber(8585871121)).toBe(true);
+    expect(functions.isPhoneNumber('8585871121')).toBe(true);
 });
 
 test('checks second valid phone number', () => {
-    expect(functions.isPhoneNumber(8585342230)).toBe(true);
+    expect(functions.isPhoneNumber('8585342230')).toBe(true);
 });
 
 test('checks first invalid phone number', () => {
-    expect(functions.isPhoneNumber(1234)).toBe(false);
+    expect(functions.isPhoneNumber('1234')).toBe(false);
 });
 
 test('checks second invalid phone number', () => {
-    expect(functions.isPhoneNumber(99)).toBe(false);
+    expect(functions.isPhoneNumber('99')).toBe(false);
 });
 
 test('checks first valid email', () => {
